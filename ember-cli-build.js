6b1c580b68fbe5ef9ffa884fa4b9bf54d8712d6d
Dan Caccavano <dcaccavano@gmail.com>
2020-06-21 20:34:18 -0700

    alters ember-cli-build and more folder cleanup

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,19 +2,6 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
-const isProduction = EmberApp.env() === 'production';
-
-const purgeCSS = {
-  module: require('@fullhuman/postcss-purgecss'),
-  options: {
-    content: [
-      // add extra paths here for components/controllers which include tailwind classes
-      './app/index.html',
-      './app/templates/**/*.hbs'
-    ],
-    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-  }
-}
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
@@ -27,8 +14,7 @@ module.exports = function(defaults) {
               path: ['node_modules']
             }
           },
-          require('tailwindcss')('./app/tailwind/config.js'),
-          ...isProduction ? [purgeCSS] : []
+          require('tailwindcss')('./app/tailwind/config.js')
         ]
       }
     }
